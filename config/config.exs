89a1ec81d59bfc8d9edc6c61f257d85aef1eacff
Mikal Villa <mikalv@mikalv.net>
2019-08-02 13:10:25 +0200

    Config update, adding environment files

--- config/config.exs
+++ config/config.exs
@@ -4,15 +4,28 @@ config :proxy,
   host: "localhost",
   port: 4480
 
+# Configures Elixir's Logger
 config :logger,
   level: :debug,
   truncate: 4096
 
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
 config :socks_server,
-  port: 4450
+  port: 4450,
+  host: "localhost"
 
 config :i2psam,
   tunnelLength: 1,
   tunnelQuantity: 5,
-  tunnelBackupQuantity: 2
+  tunnelBackupQuantity: 2,
+  signatureType: "RedDSA_SHA512_Ed25519",
+  tunnelID: "private-outproxy",
+  samHost: '127.0.0.1',
+  samPort: 7656
 
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ config/releases.exs
@@ -0,0 +1,14 @@
+import Config
+
+config :hexpm,
+  secret: System.fetch_env!("HEXPM_SECRET"),
+  private_key: System.fetch_env!("HEXPM_SIGNING_KEY"),
+  s3_bucket: System.fetch_env!("HEXPM_S3_BUCKET"),
+
+config :ex_aws,
+  access_key_id: System.fetch_env!("HEXPM_AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.fetch_env!("HEXPM_AWS_ACCESS_KEY_SECRET")
+
+config :kernel,
+  inet_dist_listen_min: String.to_integer(System.fetch_env!("BEAM_PORT")),
+  inet_dist_listen_max: String.to_integer(System.fetch_env!("BEAM_PORT"))
