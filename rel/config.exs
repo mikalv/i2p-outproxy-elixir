7a142977a8dec184a55f5c7345777e3a5b6a37d4
Mikal Villa <mikalv@mikalv.net>
2019-08-02 13:30:15 +0200

    Adding release files

---
+++ rel/config.exs
@@ -0,0 +1,68 @@
+# Import all plugins from `rel/plugins`
+# They can then be used by adding `plugin MyPlugin` to
+# either an environment, or release definition, where
+# `MyPlugin` is the name of the plugin module.
+~w(rel plugins *.exs)
+|> Path.join()
+|> Path.wildcard()
+|> Enum.map(&Code.eval_file(&1))
+
+use Mix.Releases.Config,
+  # This sets the default release built by `mix release`
+  default_release: :default,
+  # This sets the default environment used by `mix release`
+  default_environment: Mix.env()
+
+# For a full list of config options for both releases
+# and environments, visit https://hexdocs.pm/distillery/config/distillery.html
+
+# You may define one or more environments in this file,
+# an environment's settings will override those of a release
+# when building in that environment, this combination of release
+# and environment configuration is called a profile
+
+environment :dev do
+  # If you are running Phoenix, you should make sure that
+  # server: true is set and the code reloader is disabled,
+  # even in dev mode.
+  # It is recommended that you build with MIX_ENV=prod and pass
+  # the --env flag to Distillery explicitly if you want to use
+  # dev mode.
+  set(dev_mode: true)
+  set(include_erts: false)
+  set(cookie: :"nGwE?&r1S7jo<)Ef?,!JZBD*e<R[3QUv}ww4Q9~2M~pUWDSZgwZS;d@WYtUp?:bE")
+end
+
+environment :prod do
+  set(include_erts: true)
+  set(include_src: false)
+  set(cookie: :"0xT,5L%1R(bhS`32.V*.<f,E|Gw$em,|0~4,h.Erh)P@X3U`_*|zFZ/7NX9oT2]R")
+  set(vm_args: "rel/vm.args")
+
+  set(
+    config_providers: [
+      {Mix.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/config/runtime.exs"]}
+    ]
+  )
+
+  set(
+    overlays: [
+      {:copy, "config/runtime.exs", "config/runtime.exs"}
+    ]
+  )
+end
+
+# You may define one or more releases in this file.
+# If you have not set a default release, or selected one
+# when running `mix release`, the first release in the file
+# will be used by default
+
+release :app do
+  set(version: current_version(:app))
+
+  set(
+    applications: [
+      :runtime_tools
+    ]
+  )
+end
