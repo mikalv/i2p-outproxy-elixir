ae9aeec8b754ce8ad1d0674f6b2ea7d15794251c
Mikal Villa <mikalv@mikalv.net>
2019-08-02 10:59:23 +0200

    Config update

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,18 @@
 use Mix.Config
-config :proxy, host: "localhost"
-config :proxy, port: 4480
+
+config :proxy,
+  host: "localhost",
+  port: 4480
+
 config :logger,
   level: :debug,
   truncate: 4096
+
+config :socks_server,
+  port: 4450
+
+config :i2psam,
+  tunnelLength: 1,
+  tunnelQuantity: 5,
+  tunnelBackupQuantity: 2
+
